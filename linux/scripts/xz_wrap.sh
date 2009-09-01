bb611b546eea76ff098f60309647bf548bf66a25
Lasse Collin <lasse.collin@tukaani.org>
2009-09-01 23:18:41 +0300

    Make xz_wrap.sh accept one argument, which must be "kernel" or "misc".
    The idea is to use "kernel" when compressing the kernel
    image, and "misc" to compress other things like initramfs.
    "kernel" uses big dictionary and BCJ filter, while "misc"
    uses 1 MiB dictionary without BCJ.

--- linux/scripts/xz_wrap.sh
+++ linux/scripts/xz_wrap.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #
-# Wrapper for xz to use the BCJ filter of the target architecture
+# This is a wrapper for xz to use appropriate compression options depending
+# on what is being compressed. The only argument to this script should be
+# "kernel" or "misc" to indicate what is being compressed.
 #
 # Author: Lasse Collin <lasse.collin@tukaani.org>
 #
@@ -8,23 +10,36 @@
 # You can do whatever you want with this file.
 #
 
-PB=2
-case $ARCH in
-	x86|x86_64)     BCJ=--x86 ;;
-	powerpc)        BCJ=--powerpc ;;
-	ia64)           BCJ=--ia64; PB=4 ;;
-	arm)            BCJ=--arm ;;
-	sparc)          BCJ=--sparc ;;
-	*)              BCJ= ;;
-esac
+# Defaults: No BCJ filter and no extra LZMA2 options.
+BCJ=
+LZMA2OPTS=
 
-# Big dictionary is OK for the kernel image, but it's not OK for initramfs
-# or initrd.
+# Big dictionary is OK for the kernel image, but it's not OK
+# for other things.
+#
+# BCJ filter is used only for the kernel, at least for now.
+# It could be useful for non-trivial initramfs too, but it
+# depends on the exact content of the initramfs image.
 case $1 in
-	arch/*)         DICT=16MiB ;;
-	*)              DICT=1MiB ;;
+	kernel)
+		DICT=16MiB
+		case $ARCH in
+			x86|x86_64)     BCJ=--x86 ;;
+			powerpc)        BCJ=--powerpc ;;
+			ia64)           BCJ=--ia64; LZMA2OPTS=pb=4 ;;
+			arm)            BCJ=--arm ;;
+			sparc)          BCJ=--sparc ;;
+		esac
+		;;
+	misc)
+		DICT=1MiB
+		;;
+	*)
+		echo "xz_wrap.sh: Invalid argument \`$1'" >&2
+		exit 1
+		;;
 esac
 
 # This is very slow, but it should give very good compression too.
-xz --stdout --quiet --threads=1 --check=crc32 \
-		$BCJ --lzma2=preset=9e,dict=$DICT,pb=$PB "$1"
+exec xz --stdout --quiet --threads=1 --check=crc32 \
+		$BCJ --lzma2=preset=6e,$LZMA2OPTS,dict=$DICT
