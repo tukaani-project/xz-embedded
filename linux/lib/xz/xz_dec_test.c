c509816eaf6f918f2b1c44e0c574b9a5408b978a
Lasse Collin <lasse.collin@tukaani.org>
2010-11-17 21:06:42 +0200

    Use <linux/uaccess.h> instead of <asm/uaccess.h>.
    Caught by checkpatch.pl.

--- linux/lib/xz/xz_dec_test.c
+++ linux/lib/xz/xz_dec_test.c
@@ -10,7 +10,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/fs.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/crc32.h>
 #include <linux/xz.h>
 
