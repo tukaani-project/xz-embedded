f0f336e74f6615c0e93fdc54940e077886e33042
Lasse Collin <lasse.collin@tukaani.org>
2010-11-18 20:28:55 +0200

    Bump xz_dec version to 1.0.

--- linux/lib/xz/xz_dec_syms.c
+++ linux/lib/xz/xz_dec_syms.c
@@ -16,7 +16,7 @@ EXPORT_SYMBOL(xz_dec_run);
 EXPORT_SYMBOL(xz_dec_end);
 
 MODULE_DESCRIPTION("XZ decompressor");
-MODULE_VERSION("0.2");
+MODULE_VERSION("1.0");
 MODULE_AUTHOR("Lasse Collin <lasse.collin@tukaani.org> and Igor Pavlov");
 
 /*
